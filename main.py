d46911d44beb8bbedf1283459144365db350a4d6
zailerAlina <22fmif.a.zailer@std.npu.edu.ua>
2025-10-25 16:54:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import csv
 
+all_students = []
+with open('students.csv', mode='r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        all_students.append(row)
