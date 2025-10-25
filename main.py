bba26cf3075fa944707d8278ee0cdf41d521b8f2
zailerAlina <22fmif.a.zailer@std.npu.edu.ua>
2025-10-25 17:35:36 +0300

    Step 3

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import csv
-
 all_students = []
+
 with open('dataset.csv', mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
         all_students.append(row)
+
+for student in all_students:
+    grades = [
+        float (student['Основи програмування']),
+        float (student['Лінійна алгебра']),
+        float (student['Проекційна геометрія']),
+        float (student['Математичний аналіз']),
+    ]
+    s=sum(grades)
+    l=len(grades)
+    student['Середня оцінка'] = round(s//l, 2)
+
